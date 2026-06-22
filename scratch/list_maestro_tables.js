ca61b90961b9bea3f95374f826bfe15c7349ed86
ERIKA AI <erika@ferreteria.local>
2026-06-22 16:50:43 -0600

    Actualizacion automatica ERIKA

--- scratch/list_maestro_tables.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('supabase_schema_maestro.sql', 'utf8');
-const regex = /CREATE TABLE (?:IF NOT EXISTS )?(\w+)/gi;
-let match;
-while ((match = regex.exec(content)) !== null) {
-  console.log(match[1]);
-}
